6d70590cf368e3ad28051b1f4387de71ec67a89d
Piyush Wandile <98741813+prw09@users.noreply.github.com>
2024-09-20 23:13:19 +0530

    Updated

--- Queries.sql
+++ Queries.sql
@@ -124,6 +124,7 @@ WHERE
 
 -- 6 . Seat won by India alliance parties ? 
 
+	
 SELECT 
     won AS INDIA_Total_Seats_Won,
     party AS India_Alliance_Party_Name
@@ -371,4 +372,28 @@ GROUP BY
     cr.Constituency_Name
 ORDER BY 
     cr.Constituency_Name;
+
+
+-- 16 . For the state of Maharashtra, what are the total number of seats, total number of candidates, total number of parties, total votes (including EVM and postal), and the breakdown of EVM and postal votes?
+
+
+SELECT 
+    COUNT(DISTINCT cr.Constituency_ID) AS Total_Seats,
+    COUNT(DISTINCT cd.Candidate) AS Total_Candidates,
+    COUNT(DISTINCT p.Party) AS Total_Parties,
+    SUM(cd.EVM_Votes + cd.Postal_Votes) AS Total_Votes,
+    SUM(cd.EVM_Votes) AS Total_EVM_Votes,
+    SUM(cd.Postal_Votes) AS Total_Postal_Votes
+FROM 
+    constituencywise_results cr
+JOIN 
+    constituencywise_details cd ON cr.Constituency_ID = cd.Constituency_ID
+JOIN 
+    statewise_results sr ON cr.Parliament_Constituency = sr.Parliament_Constituency
+JOIN 
+    states s ON sr.State_ID = s.State_ID
+JOIN 
+    partywise_results p ON cr.Party_ID = p.Party_ID
+WHERE 
+    s.State = 'Maharashtra';
 
